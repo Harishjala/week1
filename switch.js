64f8498d0c761559ea54ab613503b1ad950c063f
harish <jalaharish4@gmail.com>
2025-01-07 12:57:41 +0530

    training

---
+++ switch.js
@@ -0,0 +1,25 @@
+let week=prompt("Enter any day:");
+switch(week){
+    case "Monday":
+    case "Tuesday":
+        {
+            console.log("football");
+        }
+        break;
+    case "Wednesday":
+    case "Thursday":
+        {        
+            console.log("cricket")
+        }
+        break;
+    case "Friday":
+    case "Saturday":
+        {
+            console.log("volley ball")
+        }
+        break;
+    case "default" :
+        {
+            console.log("holiday");
+        }
+}
---
+++ variable.js
@@ -0,0 +1,11 @@
+
+
+var a=10;
+console.log(a);
+
+let b=10;
+console.log(b);
+
+const c=10;
+console.log(c);
+
